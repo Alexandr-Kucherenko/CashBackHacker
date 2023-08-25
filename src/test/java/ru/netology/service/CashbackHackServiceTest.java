adba77c84e4fad6bfb648edebfc58851ff0fbffc
Kucherenko Alexandr <alexandr.kucherenko1977@gmail.com>
2023-08-25 17:31:23 +0400

    Branch junit with tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,42 @@
+package ru.netology.service;
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @ org.junit.Test
+    public void testRemain(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected,actual);
+
+    }
+    @ org.junit.Test
+    public void testRemainBorderZero(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(expected,actual);
+
+    }
+    @ org.junit.Test
+    public void testRemainUpperLimitValue(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(expected,actual);
+
+    }
+    @ org.junit.Test
+    public void testRemainWithCashbackHack(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int actual = service.remain(amount);
+        int expected = 200;
+        assertEquals(expected,actual);
+
+    }
+
+}
